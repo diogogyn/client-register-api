fed6af89b1d6717253d276828da83ed3e9b63537
Diogo O Santos <diogogyn100@gmail.com>
2024-03-01 15:32:57 -0300

    spring doc

---
+++ src/main/java/br/api/rest/client/register/APIClientRegister/springdoc/SpingDocConfigurations.java
@@ -0,0 +1,28 @@
+package br.api.rest.client.register.APIClientRegister.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpingDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Client Register API")
+                        .description("API Rest da aplicação Client Register, contendo as funcionalidades de CRUD de Usuarios")
+                        .contact(new Contact()
+                                .name("Time Backend")
+                                .email("backend@client.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://client.com/api/licenca")));
+    }
+}
